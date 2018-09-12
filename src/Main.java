218f79c040e17d2d3e87d2129b1e44d1ae281157
Alena <alena.a.girenko@gmail.com>
2018-09-12 13:14:01 +0300

    Modul 4 Enum, Arrays, Algorithms

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,11 @@
 import People.Employee;
+import People.Position;
+
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int b1=1, b2=0, b3=0; // the value of bonuses
+        int b1=5, b2=10, b3=0, b4=0, b5=15; // the value of bonuses %
 
 // Test default constructor:
 //        Employee emp1 = new Employee();
@@ -26,32 +29,107 @@ public class Main {
 // Test two variables constructor:
 //        Employee emp1 = new Employee("Ойры-ойры Р.", 1.5);
 //        emp1.setHours(10);
-//Test three variables constructor:
-          Employee emp1 = new Employee("Ойры-ойры Р.", 1.5, 10);
-          Employee emp2 = new Employee("Почкин В.", 1.3, 9);
-          Employee emp3 = new Employee("Привалов А.", 1.2, 8);   // summing employees hours in Constructor
+//Test five variables constructor:
+          Employee emp1 = new Employee("Ойра-ойра Р.", 1930, Position.Senior_staff_scientist, 9, 1.4);
+          Employee emp2 = new Employee("Почкин В.", 1940, Position.Research_assistant,  8, 1.0);
+          Employee emp3 = new Employee("Привалов А.", 1938, Position.Programmer, 10, 1.3);
+          Employee emp4 = new Employee("Киврин Ф.", 0, Position.Head_of_department, 8, 1.8);
+          Employee emp5 = new Employee("Камноедов М.", 1915, Position.Household_manager, 8, 1.0);
+        Employee emp6 = new Employee("Ойра-ойра Р.", 1930, Position.Head_of_department, 4, 1.4);
+        Employee emp7 = new Employee("Привалов А.", 1938, Position.Research_assistant, 4, 1.3);
 
-//Change an hour rate:
+          //Changes an hour rate:
         emp2.changeRate(1.1);
-        emp1.changeRate(1.4);
+        emp1.changeRate(1.5);
 
+                                                            // Creates array of Employee objects
+       Employee arrEmp [] = new Employee[Employee.getCountEmp()];
+       arrEmp [0] = emp1; arrEmp [1] = emp2; arrEmp [2] = emp3; arrEmp [3] = emp4; arrEmp [4] = emp5;
+        arrEmp [5] = emp6; arrEmp [6] = emp7;
 
-// Calc a day Salary:
-        double salary1 = emp1.Salary()+emp1.bonuses(b1);
-        double salary2 = emp2.Salary()+emp2.bonuses(b2);
-        double salary3 = emp3.Salary()+emp3.bonuses(b3);
+                                                        // Creates arrays with corresponding employees' parameters
+        String empName [] = {emp1.getName(), emp2.getName(), emp3.getName(), emp4.getName(),
+                emp5.getName(), emp6.getName(), emp7.getName() };
+        int empBirthYear [] = {emp1.getBirthYear(), emp2.getBirthYear(), emp3.getBirthYear(),
+                emp4.getBirthYear(), emp5.getBirthYear()};
+        int empHour [] = {emp1.getHours(), emp2.getHours(), emp3.getHours(), emp4.getHours(), emp5.getHours()};
+        double empRate [] = {emp1.getRate(), emp2.getRate(), emp3.getRate(), emp4.getRate(), emp5.getRate()};
+        double empSalary [] = {emp1.bonusSalary(b1), emp2.bonusSalary(b2), emp3.bonusSalary(b3),
+                emp4.bonusSalary(b4), emp5.bonusSalary(b5), emp6.bonusSalary(b1), emp7.bonusSalary(b3)};
 
-
-        System.out.println(emp1.toString());
-        System.out.println(emp1.getName() + " gets "+ salary1 + " a day.");
+        System.out.println(emp1.toString() + " Gets "+ empSalary [0] + " a day.");
         System.out.println();
-        System.out.println(emp2.toString());
-        System.out.println(emp2.getName() + " gets "+ salary2 + " a day.");
+        System.out.println(emp2.toString() + " Gets "+ empSalary [1] + " a day.");
         System.out.println();
-        System.out.println(emp3.toString());
-        System.out.println(emp3.getName() + " gets "+ salary3 + " a day.");
+        System.out.println(emp3.toString() + " Gets "+ empSalary [2] + " a day.");
         System.out.println();
-        System.out.println("Department has " + Employee.getTotalHours()+" employee-hours a day.");
+        System.out.println(emp4.toString() + " Gets "+ empSalary [3] + " a day.");
+        System.out.println();
+        System.out.println(emp5.toString() + " Gets "+ empSalary [4] + " a day.");
+        System.out.println();
+        System.out.println("Total hours in the Department: " + Employee.getTotalHours()+" employee-hours a day.");
+
+
+        int counter=0;                                          // Comparison of the Employee objects
+        for (int i=0; i<(arrEmp.length-1); i++){
+            for (int j=(i+1); j<arrEmp.length; j++) {
+                if (arrEmp[i].equals(arrEmp[j])) {
+                    System.out.print("Object "+ i + " == " + j+";   ");
+                    counter++;
+                }
+            }
+        }System.out.println("More than one time entered in the database "+counter +" employee(s)!" );
+
+
+                                                         // Finding the oldest employee
+        int minYear = empBirthYear[0], mY=0;
+        for (int j=1; j<empBirthYear.length; j++) {
+                if (minYear > (empBirthYear[j])&& (empBirthYear[j])!=0) {
+                    minYear = empBirthYear[j];
+                    mY=j;
+                }
+                if (empBirthYear[j-1] == 0) System.out.println("Enter birth year for employee "+empName[j-1]);
+            }
+        System.out.println(empName[mY]+" is the oldest employee.");
+
+                                                        // Sorting employees according to their bonusSalary (descent)
+
+        for (int i=0; i<(empSalary.length-1); i++){
+            double maxSal = empSalary[i];
+            for (int j=i+1; j<empSalary.length; j++) {
+                if (maxSal < (empSalary[j])) {
+                    String nameSort;
+                    maxSal = empSalary[j];
+                    empSalary[j] = empSalary[i];
+                    empSalary[i] = maxSal;
+                    nameSort = empName[j];
+                    empName[j] = empName[i];
+                    empName[i] = nameSort;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(empSalary));
+        System.out.println(Arrays.toString(empName));
+
+                                                    // Sorting employees according to their bonusSalary (increase)
+
+        for (int i=0; i<(empSalary.length-1); i++){
+            double minSal = empSalary[i];
+            for (int j=i+1; j<empSalary.length; j++) {
+                if (minSal > (empSalary[j])) {
+                    String nameSort;
+                    minSal = empSalary[j];
+                    empSalary[j] = empSalary[i];
+                    empSalary[i] = minSal;
+                    nameSort = empName[j];
+                    empName[j] = empName[i];
+                    empName[i] = nameSort;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(empSalary));
+        System.out.println(Arrays.toString(empName));
+
 
 
 
--- src/People/Employee.java
+++ src/People/Employee.java
@@ -1,71 +1,86 @@
 package People;
 
+import Module3HomeWork.Dog;
+
 public class Employee {
     private String name;
     private double rate;
     private int hours;
-    public int countEmp=0;
+    private int birthYear;
+    public static int countEmp=0;
     private static int totalHours;
+    Position position;
 
     public Employee (){                             // default constructor
         countEmp++ ;
     };
-    public Employee (String n, double r){          // two variables constructor
-        name = n;
-        rate = r;
+    public Employee (String name, int birthYear){          // two variables constructor
+        this.name = name;
+        this.birthYear = birthYear;
         countEmp++;
 
     };
-    public Employee (String n, double r, int h){   // three variables constructor
-        name = n;
-        rate = r;
-        hours = h;
+    public Employee (String name, int birthYear, Position position, int hours, double rate){   // five variables constructor with Enum
+        this.name = name;
+        this.birthYear = birthYear;
+        this.position = position;
+        this.rate = rate;
+        this.hours = hours;
         countEmp++;
         totalHours += hours;                      // Total employees-hours a day calculation
     };
 
-    public void addHours (){           // summing hours of employees created by  default constructor
-        totalHours += hours;
-            }
+    public void addHours (){ totalHours += hours; }    // summing hours of employees created by  default constructor
 
 
     public static int getTotalHours (){               // Total employees-hours a day return
         return totalHours;
     }
 
-    public void setName(String n){
-        name = n;
+    public static int getCountEmp (){               // Total employees return
+        return countEmp;
     }
-    public String getName(){
-        return name;
-    }
-    public void setRate(double r){
-        rate = r;
-    }
-    public double getRate (){
-        return rate;
-    }
-    public void setHours(int h){
-        hours = h;
-    }
-    public int getHours (){
-        return hours;
-    }
-    public double Salary () {                         // salary a day calculation (without bonuses)
-        double sl = rate * hours;
-        return sl;
-    }
-    public void changeRate (double newRate){    // Изменение почасовой ставки
+
+    public void setName(String name){this.name = name; }
+    public void setBirthYear (int birthYear) {this.birthYear=birthYear;}
+    public void setPosition (Position position){this.position = position;}
+    public void setRate(double rate){this.rate = rate; }
+    public void setHours(int hours){ this.hours = hours;}
+
+
+    public String getName(){return name;}
+    public int getBirthYear () {return birthYear;}
+    public Position getPosition (){return position;}
+    public double getRate (){return rate;}
+    public int getHours (){return hours;}
+
+    public void changeRate (double newRate){    // hourly rate change
         rate = newRate;
     }
-    public double bonuses (int bonus){          // payrise because of bonuses [int bonus]=[%]
-        double bon = bonus * Salary()/100;
+
+    public double Salary () {                         // salary a day calculation (without bonuses)
+        double sl = this.rate * this.hours;
+        return sl;
+    }
+
+    public double bonusSalary (int bonus){          // payrise because of bonuses [int bonus]=[%]
+        double bon = bonus * this.Salary()/100 + this.Salary();
         return bon;
     }
+
     public String toString() {
-        return name + ":" + " works " + hours + " hours a day," + " gets " + rate + " per/hour.";
+        return position+" " + name + " works " + hours + " hours a day," + " gets " + rate + " per hour.";
     }
 
+    public boolean equals(Object obj) {     // Overrides equals method if employees have the same parameters
+        if (this == obj) return true;
+        if (obj == null) return false;
 
-
+        if (!(getClass() == obj.getClass())) return false;
+        else {
+            Employee tmp = (Employee) obj;
+            if (tmp.name == this.name && tmp.birthYear == this.birthYear) return true;
+            else return false;
+        }
+    }
 }
