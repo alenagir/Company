0139519a16332e3fa595a0c8089d142af07dfad5
Alena <alena.a.girenko@gmail.com>
2018-09-14 12:47:32 +0300

    Separated model and service classes

--- src/Main.java
+++ src/Main.java
@@ -6,44 +6,50 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-         int bonus[] = {5,10,0,0,15,0,0}; // the value of bonuses %
+         int bonus[] = {0,0,0,0,0,0,0}; // the value of bonuses % // ПРОВЕРИТЬ ВНОСЯТ ОШИБКУ
 
           Employee emp1 = new Employee("Ойра-ойра Р.", 1930, Position.SENIORSCI, 9, 1.4);
           Employee emp2 = new Employee("Почкин В.", 1940, Position.RESEARCHASSIST,  8, 1.0);
           Employee emp3 = new Employee("Привалов А.", 1938, Position.PROGRAMMER, 10, 1.3);
           Employee emp4 = new Employee("Киврин Ф.", 1915, Position.DEPARTMENTHEAD, 8, 1.8);
           Employee emp5 = new Employee("Камноедов М.", 1915, Position.HOUSHOLDMANAG, 8, 1.0);
-        Employee emp6 = new Employee("Ойра-ойра Р.", 1930, Position.DEPARTMENTHEAD, 4, 1.4);
+        Employee emp6 = new Employee("Ойра-ойра Р.", 1930, Position.SENIORSCI, 4, 1.4);
         Employee emp7 = new Employee("Привалов А.", 1938, Position.RESEARCHASSIST, 4, 1.3);
 
-          //Changes an hour rate:
-        emp2.changeRate(1.1);
-        emp1.changeRate(1.5);
 
                                                             // Creates the array of EmployeeInfo objects
-       EmployeeInfo emp[] = new EmployeeInfo[7];
-               emp[0].addEmp(emp1); emp[1].addEmp(emp2); emp[2].addEmp(emp3); emp[3].addEmp(emp4);
-               emp[4].addEmp(emp5); emp[5].addEmp(emp6); emp[6].addEmp(emp7);
 
         Employee employee [] = {emp1, emp2, emp3, emp4, emp5, emp6, emp7};
 
+        EmployeeInfo empInfo = new EmployeeInfo();
+        empInfo.addEmp(emp1); empInfo.addEmp(emp2); empInfo.addEmp(emp3); empInfo.addEmp(emp4);
+        empInfo.addEmp(emp5); empInfo.addEmp(emp6); empInfo.addEmp(emp7);
+
+                                                            //Prints out the array of employee
         for (int i=0; i<employee.length; i++){
             System.out.println(employee [i] + " Gets "+ employee[i].bonusSalary(bonus[i]) + " a day.");
         }
-
         System.out.println("Total hours in the Department: " + Employee.getTotalHours()+" employee-hours a day.");
         System.out.println();
 
 
-        System.out.println(emp.findSameEmp());
+                                                        // Comparison of the Employee objects
+        System.out.println(empInfo.findSameEmp());
         System.out.println();
 
+                                                        // Finding the oldest employee
+        empInfo.findOldestEmp();
+        System.out.println();
+                                                    //Sorting employees  with respect to their salary with bonuses
 
-//        System.out.println(EmployeeInfo.findOldestEmp());
-//        System.out.println(EmployeeInfo.enterBirthYear);
-//          System.out.println();
+      empInfo.sortEmpSalaryDesc();
+      empInfo.print();
 
-      //  EmployeeInfo.sortEmpSalaryDesc();
+
+
+
+
+
 
 
 
--- src/service/EmployeeInfo.java
+++ src/service/EmployeeInfo.java
@@ -4,30 +4,33 @@ import model.Employee;
 
 public class EmployeeInfo {
     private  Employee employee[];
-    public  int bonus[];
+    private   int bonus[] = {0,0,0,0,0,0,0};
     private String sameEmp;
+    private int sameEmpCount;
     private   String oldestEmp;
     private   String enterBirthYear;
     private int empNomber;
 
-    public EmployeeInfo (int maxEmp){
-        this.employee = new Employee[maxEmp];
+    public EmployeeInfo (){
+        employee = new Employee[Employee.countEmp];
     }
-    public void addEmp(Employee emp){             //TA ZE FIGNYA!!!
+    public void addEmp(Employee emp){
         employee[empNomber]=emp;
         empNomber++;
     }
 
                                                         // Comparison of the Employee objects
-     public void findSameEmp() {
+     public String findSameEmp() {
          for (int i = 0; i < (Employee.countEmp - 1); i++) {
              for (int j = (i + 1); j < Employee.countEmp; j++) {
                  if (employee[i].equals(employee[j])) {
-                     sameEmp = employee[i].getName() + " == " + employee[j].getName();
+                     sameEmp = employee[i].getName() + " is entered in rows "+ i + " and "+j;
                      System.out.println(sameEmp);
+                     sameEmpCount++;
                  }
              }
-         }
+         }return sameEmpCount+ " employee(s) entered more than one time";
+
      }
 //
 
@@ -36,13 +39,11 @@ public class EmployeeInfo {
     public String findOldestEmp () {
         int minYear =employee[0].getBirthYear();
         for (int j = 1; j < Employee.countEmp; j++) {
-            if (employee[j - 1].getBirthYear() == 0) enterBirthYear="Enter birth year for employee " + employee[j - 1].getName();
-            else enterBirthYear="All birth years are entered";
-            if (minYear > (employee[j].getBirthYear()) && (employee[j].getBirthYear()) != 0) {
+            if (minYear >= (employee[j].getBirthYear()) && (employee[j].getBirthYear()) != 0) {
                 minYear = employee[j].getBirthYear();
                oldestEmp = employee[j].getName()+ " was born in "+ employee[j].getBirthYear();
+                System.out.println(oldestEmp);
             }
-
         }return oldestEmp;
     }
 
@@ -50,18 +51,25 @@ public class EmployeeInfo {
 
     public void sortEmpSalaryDesc(){
         for (int i=0; i<(Employee.countEmp); i++){
-            double maxSal = employee[i].bonusSalary(bonus[i]);
             for (int j=i+1; j<Employee.countEmp; j++) {
-                if (maxSal < (employee[j].bonusSalary(bonus[j]))) {
+                if (employee[i].bonusSalary(bonus[i]) < (employee[j].bonusSalary(bonus[j]))) {
                     Employee sort;
-                    maxSal = employee[j].bonusSalary(bonus[j]);
                     sort = employee[j];
                     employee[j] = employee[i];
                     employee[i] = sort;
+
                 }
             }
         }
     }
+
+    public void print(){
+        for (int i=0;i<Employee.countEmp;i++){
+            System.out.println(employee[i].getName()+": salary with bonuses " + employee[i].bonusSalary(bonus[i]));
+        }
+    }
+
+
 
 
 }
